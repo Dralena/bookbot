082ca683faa760ce2f5b4d6b525891c99d57acef
Carmen von Känel <103277684+Dralena@users.noreply.github.com>
2025-07-18 16:28:59 +0200

    added the first function to read a textfile

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greeting boots")
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    content = get_book_text("books/frankenstein.txt")
+    print(content)
+
+main()
